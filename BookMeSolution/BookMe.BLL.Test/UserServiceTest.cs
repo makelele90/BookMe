f0b79c927048c83db59d95311509618e147629ba
Francis Tchatchoua <f.tchatchoua@xlntelecom.co.uk>
2014-10-20 18:09:42 +0100

    initial service classes

---
+++ BookMeSolution/BookMe.BLL.Test/UserServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Rhino.Mocks;
+using NUnit.Framework;
+
+namespace BookMe.BLL.Test
+{
+  [TestFixture]
+  public class UserServiceTest
+  {
+   // private IUserService<User> _service;
+    [SetUp]
+    public void Initialise()
+    {
+      
+    }
+
+    public void CreateUserShouldReturnAnIntegerRepresentingTheUserId()
+    {
+      //int userId = _service.Create(User);
+
+      //Assert.IsTrue(userId>0);
+    }
+  }
+}
---
+++ BookMeSolution/BookMe.BLL/Interface/IUserService.cs
@@ -0,0 +1,11 @@
+﻿
+using BookMe.BLL.Test.Interface;
+using BookMe.DAL;
+
+namespace BookMe.BLL.Interface
+{
+  public interface IUserService<T>: ICrudService<T> where T:class 
+  {
+
+  }
+}
