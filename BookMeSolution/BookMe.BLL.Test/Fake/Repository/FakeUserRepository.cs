768900e61b5ecc9e8ef54edb5733f3b7b82320e7
Francis Tchatchoua <f.tchatchoua@xlntelecom.co.uk>
2014-10-21 09:37:03 +0100

    Fake repository v1

---
+++ BookMeSolution/BookMe.BLL.Test/Fake/Repository/FakeUserRepository.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using BookMe.DAL;
+using BookMe.DAL.Repository;
+
+namespace BookMe.BLL.Test.Fake.Repository
+{
+  public class FakeUserRepository:IRepository<User>
+  {
+    private readonly IQueryable<User> _users=new List<User>()
+      {
+        new User(){FirstName = "francis",LastName = "tchatchoua",Password = "123456",UserName = "admin"},
+        new User(){FirstName = "tamo",LastName = "gaspar",Password = "123456",UserName = "patdiamant"},
+        new User(){FirstName = "lion",LastName = "domou",Password = "123456",UserName = "didiversace"},
+        new User(){FirstName = "franck",LastName = "capi",Password = "123456",UserName = "catlife"},
+
+      }.AsQueryable();
+
+    public IQueryable<User> FindAll()
+    {
+      return _users;
+    }
+
+    public User Single(Expression<Func<User, bool>> predicate)
+    {
+      return _users.SingleOrDefault(predicate);
+    }
+
+    public IQueryable<User> Find(Expression<Func<User, bool>> predicate)
+    {
+      return _users.Where(predicate);
+    }
+
+    public void Add(User entity)
+    {
+      _users.ToList().Add(entity);
+    }
+
+    public void Update(User entity)
+    {
+      var user = _users.ToList().SingleOrDefault(u => u.UserName == entity.UserName);
+
+      if (user!=null)
+      {
+        _users.ToList().Remove(user);
+      }
+
+      _users.ToList().Add(entity);
+     
+    }
+
+    public void Delete(User entity)
+    {
+      var user = _users.ToList().SingleOrDefault(u => u.UserName == entity.UserName);
+
+      if (user != null)
+      {
+        _users.ToList().Remove(user);
+      }
+    }
+  }
+}
--- BookMeSolution/BookMe.DAL/BookMeContext.cs
+++ BookMeSolution/BookMe.DAL/BookMeContext.cs
@@ -1,5 +1,4 @@
 ﻿using System.Data.Entity;
-using BookMe.DAL;
 
 namespace BookMe.DAL
 {
@@ -8,8 +7,8 @@ namespace BookMe.DAL
       public BookMeContext()
         : base("BookMeConnectionString")
         {
-           // Database.SetInitializer<BlogDataContext>(new BlogDbInitializer());
-         //Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogDataContext, Configuration>("BlogConnectionString"));
+          /* Database.SetInitializer<BlogDataContext>(new BlogDbInitializer());
+         Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogDataContext, Configuration>("BookMeConnectionString"));*/
           Database.SetInitializer<BookMeContext>(null);
           
         }
@@ -23,13 +22,12 @@ namespace BookMe.DAL
             modelBuilder.Entity<User>().Property(e => e.LastName).HasMaxLength(250).IsRequired();
             modelBuilder.Entity<User>().Property(e => e.Avatar).HasMaxLength(1000);
             modelBuilder.Entity<User>().Property(e => e.UserName).IsRequired().HasMaxLength(250);
-          modelBuilder.Entity<User>().Property(e => e.Password).HasMaxLength(250).IsRequired();
+            modelBuilder.Entity<User>().Property(e => e.Password).HasMaxLength(250).IsRequired();
 
-          //Role
+            //Role
             modelBuilder.Entity<Role>().Property(e => e.Name).HasMaxLength(250).IsRequired();
 
             //category
-            
             base.OnModelCreating(modelBuilder);
 
          
